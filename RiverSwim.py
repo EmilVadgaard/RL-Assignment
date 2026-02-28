4f96f3ee3f7f19376eca5a3a3e43df0156861a1f
Emil Nielsen <emiln17@student.sdu.dk>
2026-03-01 00:29:31 +0100

    hmm..?

--- RiverSwim.py
+++ RiverSwim.py
@@ -9,82 +9,93 @@ right = np.matrix([[ 0.4,    0.6,    0,      0,      0,      0    ],
                    [ 0,      0,      0,      0.05,   0.6,    0.35 ],
                    [ 0,      0,      0,      0,      0.4,    0.6  ]])
 
-@dataclass
-class RiverSwim:
-    states: int
-    current_state: int = 0
-    first_and_last = (0.4, 0.6)
-    middle = (0.05, 0.6, 0.35)
 
-    def __init__( self, states: int = 6 ):
-        self.states = states
-        self.actions = ("LEFT", "RIGHT")
-    
-    # returns (new state, reward)
-    def step(self, action: str) -> (int, int):
-        if action == "RIGHT":
-            if self.current_state == 0:
-                if random() < self.first_and_last[0]:
-                    self.printState()
-                    return self.current_state, 0
-                else:
-                    self.current_state = self.current_state + 1
-                    self.printState()
-                    return self.current_state, 0
-                
-            elif self.current_state == self.states - 1:
-                if random() < self.first_and_last[0]:
-                    self.current_state = self.current_state - 1
-                    self.printState()
-                    return self.current_state, 0
-                else:
-                    self.printState()
-                    return self.current_state, 1
-                
+states: int = 6
+first_and_last = (0.4, 0.6)
+middle = (0.05, 0.6, 0.35)
+
+# returns (new state, reward)
+# Should also take state as input????
+def step(old_state: int, action: str) -> (int, int):
+    rnd = random()
+    if action == "RIGHT":
+        if old_state == 0:
+            if rnd < first_and_last[0]:
+                printState()
+                return old_state, 0
             else:
-                if random() < self.middle[0]:
-                    self.current_state = self.current_state - 1
-                    self.printState()
-                    return self.current_state, 0
-                elif random() < self.middle[1]:
-                    self.printState()
-                    return self.current_state, 0
-                else:
-                    self.current_state = self.current_state + 1
-                    self.printState()
-                    return self.current_state, 0
-                
-        elif action == "LEFT":
-            if self.current_state == 0:
-                self.printState()
-                return self.current_state, 5/1000
-            self.current_state = self.current_state - 1
-            self.printState()
-            return self.current_state, 0
-        
-    def printState(self):
-        #print("Current state: " + str(self.current_state))
-        return
+                new_state = old_state + 1
+                printState()
+                return new_state, 0
+            
+        elif old_state == states - 1:
+            if rnd < first_and_last[0]:
+                new_state = old_state - 1
+                printState()
+                return new_state, 0
+            else:
+                printState()
+                return old_state, 1
+            
+        else:
+            if rnd < middle[0]:
+                new_state = old_state - 1
+                printState()
+                return new_state, 0
+            elif rnd < middle[0] + middle[1]:
+                printState()
+                return old_state, 0
+            else:
+                new_state = old_state + 1
+                printState()
+                return new_state, 0
+            
+    elif action == "LEFT":
+        if old_state == 0:
+            printState()
+            return old_state, 5/1000
+        new_state = old_state - 1
+        printState()
+        return new_state, 0
     
+def printState():
+    #print("Current state: " + str(self.current_state))
+    return
+
+#AGENT PART
+
+# (old state, reward, new state)
+STATE_ACTION = np.zeros((6, 2))
+TRANSITION = np.zeros((6, 2, 6))
+REWARD = np.zeros((6, 2))
+
+#agent = (state, action) -> new state, reward
+state = 0
+while True:
+    
+    pass
 
 #Test stuff
-riverSwim1 = RiverSwim(60)
-riverSwim2 = RiverSwim(60)
-
-def leftOnly(riverSwim1) -> int:
+def leftOnly() -> int:
+    state = 0
     reward = 0
     for i in range(1000):
-        reward = reward + riverSwim1.step("LEFT")[1]
+        r = step(state, "LEFT")
+        reward = reward + r[1]
+        state = r[0]
 
     print("Reward for left only: " + str(reward))
 
-def rightOnly(riverSwim2) -> int:
+def rightOnly() -> int:
+    state = 0
     reward = 0
     for i in range(1000):
-        reward = reward + riverSwim2.step("RIGHT")[1]
+        r = step(state, "RIGHT")
+        reward = reward + r[1]
+        state = r[0]
 
     print("Reward for right only: " + str(reward))
 
 
-leftOnly(RiverSwim())
-rightOnly(RiverSwim())
+leftOnly()
+rightOnly()
